80967d40ca526d05f69944ff3a4d4e8009580d9b
na6im <nassim.amokrane@hotmail.com>
2020-04-30 05:56:58 +0100

    Create Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import { Container, Question, ProgressBar } from './styles'
+import {
+  Container,
+  QuestionLayout,
+  ProgressBar,
+  Body,
+  Title,
+  Category,
+  Question,
+  QuestionCore,
+  AnswerButton,
+  Score,
+  ScoreStat,
+  ScoreBar
+} from './styles'
 
 function App () {
   return (
     <Container>
-      <Question>
+      <QuestionLayout>
         <ProgressBar ratio={0.5} />
-      </Question>
+        <Body>
+          <Title>Question 16 of 20</Title>
+          <Category>Entertaiment: Board Games</Category>
+          <Question>
+            <QuestionCore style={{ gridArea: 'question' }}>
+              Question testQuestion testQuestion testQuestion testQuestion
+              testQuestion testQuestion testQuestion test
+            </QuestionCore>
+            <AnswerButton style={{ gridArea: 'answera' }}>
+              Question test
+            </AnswerButton>
+          </Question>
+
+          <Score>
+            <ScoreStat>
+              <div>Score 60%</div>
+              <div>Max score 75%</div>
+            </ScoreStat>
+            <ScoreBar />
+          </Score>
+        </Body>
+      </QuestionLayout>
     </Container>
   )
 }
--- src/styles.js
+++ src/styles.js
@@ -9,7 +9,9 @@ export const Container = styled.div`
   align-items: center;
 `
 
-export const Question = styled.div`
+export const QuestionLayout = styled.div`
+  display: flex;
+  flex-direction: column;
   height: 90%;
   width: 40%;
   background-color: white;
@@ -17,8 +19,75 @@ export const Question = styled.div`
 `
 
 export const ProgressBar = styled.div`
-  align-self: flex-start;
-  height: 0.8em;
+  height: 2%;
   width: ${props => props.ratio * 100}%;
   background-color: #a9aaa9;
+`
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin: 3em 5em 1em 5em;
+`
+
+export const Title = styled.div`
+  font-size: 32px;
+  font-weight: bolder;
+  color: #4a4a52;
+`
+
+export const Category = styled.div`
+  font-size: 14px;
+  color: #b1acb2;
+`
+
+export const Question = styled.div`
+  display: grid;
+  margin-top: 3em;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-areas:
+    'question question'
+    'answera answerb'
+    'answerc answerd';
+  grid-column-gap: 2em;
+  grid-row-gap: 2em;
+`
+
+export const QuestionCore = styled.div`
+  grid-area: 'question';
+  font-weight: 700;
+  font-size: 18px;
+`
+
+export const AnswerButton = styled.div`
+  grid-area: 'question';
+  background-color: #e5e6e5;
+  border-radius: 5px;
+  font-weight: 500;
+  font-size: 16px;
+  height: fit-content;
+  border: 1px solid black;
+  padding: 5px;
+  text-align: center;
+`
+
+export const Score = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 4em;
+  width: 100%;
+  border-radius: 5px;
+  margin-top: auto;
+`
+
+export const ScoreStat = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const ScoreBar = styled.div`
+  height: 100%;
+  border-radius: 5px;
+  border: solid 1px black;
 `
