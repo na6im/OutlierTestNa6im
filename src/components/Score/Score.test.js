70eb427cc5d3116c84e939555294728a0c12a7ef
na6im <nassim.amokrane@hotmail.com>
2020-05-20 07:07:46 +0100

    snapshots

--- src/components/Score/Score.test.js
+++ src/components/Score/Score.test.js
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react'
 import Score from './ScoreComponent'
 import React from 'react'
 import ScoreComponent from './ScoreComponent'
+import QuestionComponent from '../Question/QuestionComponent'
 
 test('renders corectly', () => {
   const { getAllByTestId, getByText } = render(
@@ -11,4 +12,10 @@ test('renders corectly', () => {
   expect(getAllByTestId('bar').length).toBe(3)
   expect(getByText('Score 50%').length)
   expect(getByText('Max score 95%').length)
+})
+test('Should render and match the snapshot', () => {
+  const {
+    container: { firstChild }
+  } = render(<Score score={1} total={20} totalAnswerd={2} />)
+  expect(firstChild).toMatchSnapshot()
 })
